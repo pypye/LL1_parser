701272d6331f30af308b463d2de9c373f08e4ce8
Duc Tran <69345080+pypye@users.noreply.github.com>
2023-05-10 19:23:08 +0700

    add bracket view

--- ast_print.py
+++ ast_print.py
@@ -1,18 +1,18 @@
 import os
-def remove_eps(tree, node, depth):
+def remove_eps(tree, node):
 
     for child in tree.children(node.identifier):
         if child.tag == "EPSILON":
             tree.remove_node(node.identifier)
             return tree
         
-        tree = remove_eps(tree, child, depth + 1)
+        tree = remove_eps(tree, child)
     
     return tree
 
-def remove_parent(tree, node, depth):
+def remove_parent(tree, node):
     for child in tree.children(node.identifier):
-        tree = remove_parent(tree, child, depth + 1)
+        tree = remove_parent(tree, child)
     
     if len(tree.children(node.identifier)) == 1:
         if tree.parent(node.identifier) != None:
@@ -22,13 +22,31 @@ def remove_parent(tree, node, depth):
     return tree
 
 def pretty_print_ast(file, ast):
-    ast2 = remove_eps(ast, ast.get_node(ast.root), 0)
-    ast3 = remove_parent(ast2, ast2.get_node(ast2.root), 0)
+    ast2 = remove_eps(ast, ast.get_node(ast.root))
+    ast3 = remove_parent(ast2, ast2.get_node(ast2.root))
     if os.path.exists(f"./output/{file}/out_ast_reduced.txt"):
         os.remove(f"./output/{file}/out_ast_reduced.txt")
     ast3.save2file(f"./output/{file}/out_ast_reduced.txt", key=lambda x: x.identifier)
     return ast3
 
 
+def gen_bracket(tree, node):
+    strs = "( "
+    for child in sorted(tree.children(node.identifier), key=lambda x: x.identifier):
+        if not child.tag.startswith("<"):  
+            if child.tag == "(":
+                strs += "\\( "
+            elif child.tag == ")":
+                strs += "\\) "
+            else:
+                strs += child.tag + " "
+        strs += gen_bracket(tree, child)
+    strs += ") "
+    return strs
+
 def bracket_print(file, ast):
-    pass
+    strs = gen_bracket(ast, ast.get_node(ast.root))
+    strs = strs.replace("( )", "")
+    strs = strs.replace("  ", " ")
+    out = open(f"./output/{file}/out_ast_bracket.txt", "w")
+    out.write(strs)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from parser.LL1_Paser import LL1_parser
 from ast_print import pretty_print_ast, bracket_print
 
 if __name__ == "__main__":
-    file = "in"
+    file = "example_fib"
     generate_token(file)
     ast = LL1_parser(file)
     ast = pretty_print_ast(file, ast)
